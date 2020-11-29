530523cede87a7693d3a1a497bab4a1408f6f091
Rushikesh Chaudhari <rushichaudhari1237@gmail.com>
2020-11-30 02:18:20 +0530

    ex05: wired output div

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var btn = document.querySelector("#btn");
 btn.addEventListener("click",function btnHandler(){
 	txtIn = document.querySelector("#txtInput");
-	var userInput = txtIn.value;
-	console.log(userInput);
+	divOut = document.querySelector("#divOut");
+	divOut.innerText = txtIn.value;	
 });
